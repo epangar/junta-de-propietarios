49e2212fb943f59e33fbf1add5e6ca5deae2d02e
Estanislao Pan García <vn.estanislao.pan@stlexternals.com>
2026-05-13 11:40:24 +0200

    Enrutado

--- frontend/junta-de-propietarios/src/app/app.routes.ts
+++ frontend/junta-de-propietarios/src/app/app.routes.ts
@@ -4,12 +4,16 @@ import { LoginComponent } from './components/login/login';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password';
 import { MainScreen } from './components/main-screen/main-screen';
 import { ManageUsersComponent } from './components/manage-users/manage-users';
+import { HistoryComponent } from './components/history/history.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'home', component: MainScreen},
   { path: 'manage-users', component: ManageUsersComponent },
+  { path: 'summary', component: SummaryComponent },
+  { path: 'history', component: HistoryComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
